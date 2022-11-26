2e48ec3a5508bfee7f15ac72865d29557f7e8a56
Govi <govi18051969@gmail.com>
2022-11-26 14:41:10 +0530

    Fixed Git Ignore File

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ var corsOptions = {
     optionsSuccessStatus: 200, // For legacy browser support
 };
 
-app.use("/", express.static("./build/"));
+app.use("/", express.static("../client/build"));
 app.use(express.json());
 // app.use(cors(corsOptions));
 app.use(cookieParser());
